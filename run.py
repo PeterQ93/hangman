2246ec4be7c58c3d45bcdec19847e08f2d5c3317
PeterQ93 <pq1993@live.ie>
2023-01-08 12:41:06 +0000

    add words.py file and define get_word funtction to use random words

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from words import word_list
+import random
+
+
+
+def get_word():
+    """
+    Fetches random word from words.py and returns it in capital letters
+    """
+    word = random.choice(word_list)
+    return word.upper()
+    
