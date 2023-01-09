b73db22b2ef3d7e871ae1492c4b0cf52b7820f46
PeterQ93 <pq1993@live.ie>
2023-01-09 19:15:15 +0000

    add the rules function

--- run.py
+++ run.py
@@ -2,11 +2,53 @@ from words import word_list
 import random
 
 
-
 def get_word():
     """
     Fetches random word from words.py and returns it in capital letters
     """
-    word = random.choice(word_list)
-    return word.upper()
+    random_word = random.choice(word_list)
+    return random_word.upper()
+
+
+def homepage():
+    """
+    Homepage the user is first brought to.
+    Gives them the option to start the game or to read the rules.
+    """
+    word = get_word()
+    homepage_graphic()
+    print(display_hangman(0))
+    print('Type 1 to begin game\n')
+    print('Type 2 to read the instructions')
+    selection = False
+    while not selection:
+        choice = input(' \n')
+        if choice == '1':
+            selection = True
+            start_game(word)
+        elif choice == '2':
+            selection = True
+            rules()
+        else:
+            print('\n Please type 1 or 2 to make your choice.')
+
+
+def rules():
+    """
+    Display instructions for play the game.
+    """
+    print('1. Try to guess the word by typing 1 letter at a time.')
+    print('2. Every wrong answer will cost you one life.')
+    print('3. The Hanging will begin once a life is lost')
+    print('4. Once you run out of lives its gameover')
+    print('5. Win the game by guessing the correct letters')
+    print('6. Good Luck you will need it!')
+         
+
+    start = input('Press the enter key to return to the Homepage.\n')
+    homepage()
+
+
+
     
+
